72e7d13a0583a7781c6ee38b639c0197bd50c554
aizharyk <aizharolexin@yandex.kz>
2025-12-14 15:30:03 +0500

    feat: add RpcHandlerContract.php to improve RPC handling functionality

---
+++ src/Contracts/RpcHandlerContract.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OlexinPro\Centrifuge\Contracts;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use RoadRunner\Centrifugo\Request\RPC;
+
+interface RpcHandler
+{
+    /**
+     * Handle RPC request
+     *
+     * @param array<string, mixed> $data Request data
+     * @param Authenticatable|null $user Authenticated user or null
+     * @param RPC $request Original RPC request
+     * @return mixed Response data (will be serialized to JSON)
+     */
+    public function handle(array $data, ?Authenticatable $user, RPC $request): mixed;
+}
